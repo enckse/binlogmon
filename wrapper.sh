49aaa0023870e14e0bfbc263580d285181dd1d33
Sean Enck <enckse@gmail.com>
2015-12-30 10:52:18 -0500

    Make sure the log directory exists before operating and assume we're in the 'right' place when being executed

--- wrapper.sh
+++ wrapper.sh
@@ -4,11 +4,11 @@ PATH_TO_LOGS=$LOG_FILES
 
 # Share location of the log file, we'll copy it before doing any reading
 PAST_LOGS=$LOCATION/logs/
+mkdir -p $PAST_LOGS
 TIMESTAMP=$(date +%s)
 LOG_COPY=$PAST_LOGS$TIMESTAMP.log
 
-# Move and update
-cd $LOCATION/binlogmon.git
+# Assuming we're in the 'correct' place to refresh
 git pull &>/dev/null
 
 # Prep for actual execution
