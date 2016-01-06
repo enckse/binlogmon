0e64828b8a477e9b852cc1f4d0f663f145889e66
Sean Enck <enckse@gmail.com>
2016-01-06 10:56:32 -0500

    Require a specific version of twilio

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url='https://github.com/epiphyte/binlogmon',
     license='MIT',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    install_requires=['twilio'],
+    install_requires=['twilio==5.2.0'],
     entry_points={
         'console_scripts': [
             'binlogmon=binlogmon:main',
